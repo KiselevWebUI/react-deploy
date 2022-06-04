cde92c28b05c6a34bd99a248dd43afa0905144ff
Kiselev Andrey <web-ui@mail.ru>
2022-06-05 01:00:15 +0300

    deploy my react add to github pages

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <h1>My react app</h1>
+      <h2>Author: akiselev</h2>
+    </div>
+  );
+}
+
+export default App;
